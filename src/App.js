7a42b6c5b73ee9e4dd1d4897a74ca89cf2a0bd7d
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-25 05:57:56 +0200

    Functions and component building structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-function App() {
+import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlace";
+const App = () => {
   return (
-    <div>
-      <h1> APP </h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route to="/" component={Users} />{" "}
+        <Route to="/new/places" component={NewPlaces} /> <Redirect to="/" />
+      </Switch>{" "}
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,17 @@
+import React from "react";
+import UsersList from "../components/UsersList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Youssef",
+      image:
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
