aaff69aed66a71deefac207c766b50b4b866ed91
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-26 03:31:29 +0200

    add sidebar drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,29 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import SideDrawer from "./SideDrawer";
 import NavLinks from "./NavLinks";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawIsOpen, setDrawIsOpen] = useState(false);
+
   return (
     <Fragment>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawIsOpen && <Backdrop onClick={() => setDrawIsOpen(!drawIsOpen)} />}
+      {drawIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={() => setDrawIsOpen(!drawIsOpen)}
+        >
           <span />
           <span />
           <span />
