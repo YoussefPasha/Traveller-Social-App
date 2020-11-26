0994160d01ad0c2f2defa6a976055cb311a6661b
Youssef Pasha <jojoyossefbasha010@gmail.com>
2020-11-26 04:26:01 +0200

    ui and cycle for user experience

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+  const content = (
+    <CSSTransition>
+      <aside className="side-drawer">{props.children}</aside>
+    </CSSTransition>
+  );
   return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
 
