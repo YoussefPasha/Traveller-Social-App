166bd6723036da048b175bcd6162a21785dcddae
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-19 03:28:04 +0200

    Environment Variables in React Apps

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -2,17 +2,23 @@ import * as React from "react";
 import { Component } from "react";
 import ReactMapGL, { GeolocateControl } from "react-map-gl";
 
+let secretKey = process.env.REACT_APP_MAP_BOX_SECRET_KEY;
+
 export default class Map extends Component {
   state = {
-    viewport: { longitude: this.props.center.lng, latitude: this.props.center.lat, zoom: 14 },
+    viewport: {
+      longitude: this.props.center.lng,
+      latitude: this.props.center.lat,
+      zoom: 14,
+    },
   };
 
   render() {
     const { viewport } = this.state;
     return (
       <ReactMapGL
-      {...viewport}
-        mapboxApiAccessToken="pk.eyJ1IjoieW91c3NlZnBhc2hhIiwiYSI6ImNraHo5MnRkbTBkOGEyenFoazgwMW8yM3UifQ.cJ7c1h0Ii_JsyZYPsSG1rQ"
+        {...viewport}
+        mapboxApiAccessToken={secretKey}
         width="100vw"
         height="100vh"
         onViewportChange={(viewport) => this.setState({ viewport })}
